549fbbb2bc4c9aeeb6d73e4316cc5174035290c2
PaulR <prarobinson@gmail.com>
2020-05-28 15:10:12 -0700

    updated queries3x to include csv dump

--- queries3x.py
+++ queries3x.py
@@ -22,6 +22,7 @@ def main(dbname, user):
 		prompt += "\t10) Dive area surveyed by substrate\n"
 		prompt += "\t11) Plot CTD data\n"
 		prompt += "\t12) Logistical information for dives by cruise\n"
+		prompt += "\t13) Dump database as .csv files\n"
 		prompt += "--> "
 		
 		while True:
@@ -231,10 +232,17 @@ def main(dbname, user):
 					print(row)
 				response = input("\nPlease enter a cruise_id from the above: ")
 				cruise_id = (response.strip(),)
-				SQL="select dive. , from dive where dive.cruise_id= %s);"
-				cursor.execute(SQL, cruise_id)
+				print('This function is not implemented yet...')
+				#SQL="select dive. , from dive where dive.cruise_id= %s);"
+				#cursor.execute(SQL, cruise_id)
 
-			print()
+
+
+			elif choice == 13:
+				response = input("\nPlease enter an output directory with write permissions: ")
+				output_dir = (response.strip(),)
+				SQL="CREATE OR REPLACE FUNCTION db_to_csv(path TEXT) RETURNS void AS $$ declare tables RECORD;   statement TEXT; begin FOR tables IN SELECT (table_schema || '.' || table_name) AS schema_table FROM information_schema.tables t INNER JOIN information_schema.schemata s ON s.schema_name = t.table_schema WHERE t.table_schema NOT IN ('pg_catalog', 'information_schema') AND t.table_type NOT IN ('VIEW') ORDER BY schema_table LOOP statement := 'COPY ' || tables.schema_table || ' TO ''' || path || '/' || tables.schema_table || '.csv' ||''' DELIMITER '';'' CSV HEADER'; EXECUTE statement; END LOOP; return; end; $$ LANGUAGE plpgsql; SELECT db_to_csv(%s);"
+				cursor.execute(SQL, output_dir)
 		
 	finally:
 		conn.close()
