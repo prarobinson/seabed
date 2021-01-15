e1e705a9435633ccc7edd07178f976de3314dae9
Curt Whitmire <curt.whitmire@noaa.gov>
2021-01-14 21:46:20 -0800

    Update Upload_species.sql
    Updated names of fields to match latest version of master species table

--- Upload_species.sql
+++ Upload_species.sql
@@ -1,11 +1,11 @@
 -- NOTE: The organism histogram query interrogates the Species_Table.csv directly, but we may wish to have this as a table instead. This table IS used to create fish_types.py for 12RB
 CREATE TABLE seabed.species_list(
   ID SERIAL,
-  ObjectID INTEGER,
-  AphiaID INTEGER,
-  RACEcode INTEGER,
-  FRAMcode INTEGER,
-  SWFSCcode VARCHAR,
+  Ref_ID INTEGER,
+  Aphia_ID INTEGER,
+  RACE_code INTEGER,
+  FRAM_code INTEGER,
+  HEP_code VARCHAR,
   Kingdom VARCHAR,
   Phylum VARCHAR,
   Subphylum VARCHAR,
@@ -19,18 +19,19 @@ CREATE TABLE seabed.species_list(
   Common_name VARCHAR,
   Common_name_authority VARCHAR,
   AUV_nickname VARCHAR,
-  Morpho_type_descriptor VARCHAR,
-  Also_called_historically_3 VARCHAR,
-  Button_12RB VARCHAR,
-  Group1 VARCHAR,
-  Group2 VARCHAR,
-  Activation_date DATE,
-  Deactivation_date DATE,
+  Also_called_historically VARCHAR,
+  12RB_category VARCHAR,
+  12RB_category_abbr VARCHAR,
+  12RB_layout VARCHAR,
+  12RB_button VARCHAR,
+  Grp_nonstandard_taxon VARCHAR,
+  Date_activated DATE,
+  Date_deactivated DATE,
   Notes VARCHAR,
   PRIMARY KEY (ID)
 );
 
 -- NOTE!!! Change the path to wherever your copy of the Species_Table.csv file lives
-COPY  species_list(ObjectID,AphiaID,RACEcode,FRAMcode,SWFSCcode,Kingdom,Phylum,Subphylum,Class,Subclass,"Order",Family,Genus,Species,AUV_OTU,common_name,common_name_authority,AUV_nickname,morpho_type_descriptor,also_called_historically_3,Button_12RB,Group1,Group2,Activation_date,Deactivation_date,Notes)
- FROM '/home/paulr/WorkShtoof/NOAA/seabed/Species_Table.csv' DELIMITER ',' CSV HEADER;
+COPY  species_list(Ref_ID,Aphia_ID,RACE_code,FRAM_code,HEP_code,Kingdom,Phylum,Subphylum,Class,Subclass,"Order",Family,Genus,Species,AUV_OTU,common_name,common_name_authority,AUV_nickname,Also_called_historically,12RB_category,12RB_category_abbr,12RB_layout,12RB_button,Grp_nonstandard_taxon,Date_activated,Date_deactivated,Notes)
+ FROM '/home/paulr/WorkShtoof/NOAA/seabed/AUV_Species_Table.csv' DELIMITER ',' CSV HEADER;
 
